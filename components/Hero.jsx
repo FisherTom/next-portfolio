0c683c42b8e78f6bcbca1426b11df7553f23f96c
fishertom <tfisher92@gmail.com>
2023-04-12 10:57:03 +0100

    project grid and grid card component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,9 +4,10 @@ function Hero() {
   return (
     <div id="home" className="w-full h-screen text-center">
       <div className=" max-w-[1240px] w-full h-full mx-auto p-2 flex items-center justify-center">
-        <div className="text-gray-800">
-          <h1 className="p-4">Tom Fisher</h1>
-          <h2>Front-end Developer</h2>
+        <div className="flex flex-col gap-4 text-gray-800">
+          <h1>Tom Fisher</h1>
+          <h2>Full-stack web Developer</h2>
+          <p>I am a somthing, please give me a job.</p>
         </div>
       </div>
     </div>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import beerImg from "../public/assests/brew-log.png";
+import ProjectItem from "./ProjectItem";
 
 function Projects() {
   return (
-    <div id="projects" className="w-full h-screen text-center">
-      <div className=" max-w-[1240px] w-full h-full mx-auto p-2 flex items-center justify-center">
-        <div className="text-gray-800">
-          <h1 className="p-4">Projects</h1>
+    <div id="projects" className="w-full ">
+      <div className="max-w-[1240px] mx-auto px-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Projects
+        </p>
+        <h2 className="py-4">What I've built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title="Brew Log App"
+            backgroundImg={beerImg}
+            projectUrl="/brewlog"
+          />
+          <ProjectItem
+            title="Brew Log App"
+            backgroundImg={beerImg}
+            projectUrl="/brewlog"
+          />
+          <ProjectItem
+            title="Brew Log App"
+            backgroundImg={beerImg}
+            projectUrl="/brewlog"
+          />
+          <ProjectItem
+            title="Brew Log App"
+            backgroundImg={beerImg}
+            projectUrl="/brewlog"
+          />
+          <ProjectItem
+            title="Brew Log App"
+            backgroundImg={beerImg}
+            projectUrl="/brewlog"
+          />
         </div>
       </div>
     </div>
